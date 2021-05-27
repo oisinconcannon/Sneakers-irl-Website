823d1d26cddb690265129f3f774a37b06cb0847c
Oisin <G00347603@gmit.ie>
2021-05-28 00:28:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Sneakers IRL
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/sneakers.irl/?hl=en"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my Instagram!
         </a>
       </header>
     </div>
